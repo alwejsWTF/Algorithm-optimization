6825ab33c6e60edd3eefc702595a5d0c60b1dd28
Marcin Strzelec <m.strzelec21052003@gmail.com>
2024-03-17 13:24:47 +0100

    add all functions

--- differential_evolution.py
+++ differential_evolution.py
@@ -1,28 +1,40 @@
 import numpy as np
-import functions as fun
+from functions import choose_fun
 
 
+#  DE/current/2/bin strategy
 class DifferentialEvolution:
-    def __init__(self, function, bounds, population_size=50, F=0.5, CR=0.9, max_generations=1000, verbose=True):
+    def __init__(self, function, scope, dimension, population_size=50, F=0.5,
+                 CR=0.9, generations=1000, verbose=True):
         self.function = function
-        self.bounds = np.asarray(bounds)
+        self.bounds = np.tile(scope, (dimension, 1))
         self.population_size = population_size
         self.F = F  # Differential weight
         self.CR = CR  # Crossover probability
-        self.max_generations = max_generations
+        self.generations = generations
         self.verbose = verbose
-        self.dimension = self.bounds.shape[1]  # Number of dimensions
-        self.population = np.random.uniform(low=self.bounds[0], high=self.bounds[1],
-                                           size=(self.population_size, self.dimension))
-        self.fitness = np.asarray([self.function(ind) for ind in self.population])
+        self.dimension = dimension
+        # Initialize the population within the bounds
+        self.population = np.random.uniform(low=self.bounds[:, 0],
+                                            high=self.bounds[:, 1],
+                                            size=(self.population_size,
+                                                  self.dimension))
+        # Calculate the fitness of the initial population
+        self.fitness = np.asarray(
+            [self.function(ind) for ind in self.population])
+        # Find the index of the best solution
         self.best_idx = np.argmin(self.fitness)
+        # Store the best solution and its fitness
         self.best_solution = self.population[self.best_idx]
         self.best_fitness = self.fitness[self.best_idx]
 
     def mutate(self, target_idx):
-        idxs = [idx for idx in range(self.population_size) if idx != target_idx]
-        r1, r2, r3, r4 = self.population[np.random.choice(idxs, 4, replace=False)]
-        mutant_vector = r1 + self.F * (r1 - r2) + self.F * (r3 - r4) #self.population[target_idx]
+        idxs = [idx for idx in range(self.population_size) if
+                idx != target_idx]
+        r1, r2, r3, r4 = self.population[
+            np.random.choice(idxs, 4, replace=False)]
+        mutant_vector = self.population[target_idx] + self.F * (
+                    r1 - r2) + self.F * (r3 - r4)
         return mutant_vector
 
     def recombine(self, target_vector, mutant_vector):
@@ -40,21 +52,24 @@ class DifferentialEvolution:
                 self.best_solution = trial_vector
 
     def optimize(self):
-        for generation in range(self.max_generations):
+        for generation in range(self.generations):
             for i in range(self.population_size):
                 mutant_vector = self.mutate(i)
-                trial_vector = self.recombine(self.population[i], mutant_vector)
-                trial_vector = np.clip(trial_vector, self.bounds[0], self.bounds[1])
+                trial_vector = self.recombine(self.population[i],
+                                              mutant_vector)
+                trial_vector = np.clip(trial_vector, self.bounds[:, 0],
+                                       self.bounds[:, 1])
                 self.select(i, trial_vector)
 
-            if self.verbose and generation % 100 == 0:
-                print(f"Generation {generation}: Best Fitness = {self.best_fitness}")
+            if self.verbose and (generation + 1) % 100 == 0:
+                print(f"Generation {generation + 1}: "
+                      f"Best Fitness = {self.best_fitness}")
 
         return self.best_solution, self.best_fitness
 
 
 # Example usage:
-bounds = (np.full(30, -100), np.full(30, 100))
-de_optimizer = DifferentialEvolution(function=fun.trid_function, bounds=bounds)
+f, scope = choose_fun(5)
+de_optimizer = DifferentialEvolution(function=f, scope=scope, dimension=30)
 best_solution, best_fitness = de_optimizer.optimize()
 print(f"Best solution found:\n{best_solution}\nBest fitness: {best_fitness}")
--- functions.py
+++ functions.py
@@ -1,18 +1,46 @@
 import numpy as np
 
-def trid_function(x):
-    sum_squares = np.sum((x - 1)**2)
-    sum_product = np.sum(x[:-1] * x[1:])
-    return sum_squares - sum_product
 
-
-def rosenbrock_function(x):
-    return np.sum(100.0 * (x[1:] - x[:-1] ** 2.0) ** 2.0 + (1 - x[:-1]) ** 2.0)
+def choose_fun(flag_fun, flag_scope=True):
+    match flag_fun:
+        case 1:
+            choose = rastrigin_function, (-5.12, 5.12)
+        case 2:
+            choose = rosenbrock_function, (-2048, 2048)
+        case 3:
+            choose = sphere_function, (-100, 100)
+        case 4:
+            choose = step_function, (-10, 10)
+        case 5:
+            choose = sum_power_function, (-1, 1)
+        case 6:
+            choose = bohachevsky_function, (-10, 10)
+        case _:
+            raise ValueError("Wrong function number")
+    return choose if flag_scope else choose[0]
 
 
 def rastrigin_function(x):
-    return 10 * len(x) + np.sum(x**2 - 10 * np.cos(2 * np.pi * x))
+    return 10 * len(x) + sum(
+        x[i] ** 2 - 10 * np.cos(2 * np.pi * x[i]) for i in range(len(x)))
+
+
+def rosenbrock_function(x):
+    return sum(100 * (x[i + 1] - x[i] ** 2) ** 2 + (1 - x[i]) ** 2 for i in
+               range(len(x) - 1))
 
 
 def sphere_function(x):
-    return np.sum(x ** 2)
+    return sum(i ** 2 for i in x)
+
+
+def step_function(x):
+    return sum((i + 0.5) ** 2 for i in x)
+
+
+def sum_power_function(x):
+    return sum(np.abs(x[i]) ** (i + 1) for i in range(len(x)))
+
+
+def bohachevsky_function(x):
+    return 418.9829 * len(x) - sum(i * np.sin(np.sqrt(np.abs(i))) for i in x)
