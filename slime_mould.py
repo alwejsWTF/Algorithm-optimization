bcef91fdaeea351d32a8101a21c3bb7d3bec928e
Marcin Strzelec <m.strzelec21052003@gmail.com>
2024-05-31 11:17:46 +0200

    Add migration to SMA

--- slime_mould.py
+++ slime_mould.py
@@ -4,7 +4,7 @@ from functions import choose_fun
 
 class MultiSwarmSMA:
     def __init__(self, function, scope, dimension, swarm_size, iterations,
-                 num_swarms, z, verbose):
+                 num_swarms, z, migration_threshold, verbose):
         self.function = function
         self.scope = scope
         self.dimension = dimension
@@ -12,6 +12,7 @@ class MultiSwarmSMA:
         self.iterations = iterations
         self.num_swarms = num_swarms
         self.z = z
+        self.migration_threshold = migration_threshold
         self.verbose = verbose
         self.swarms = [np.random.uniform(
             low=scope[0], high=scope[1], size=(swarm_size, dimension)
@@ -32,26 +33,28 @@ class MultiSwarmSMA:
     def optimize(self):
         best_values_per_iteration = []
         for i in range(self.iterations):
-            for swarm_idx, swarm in enumerate(self.swarms):
+            for swarm_idx in range(self.num_swarms):
+                swarm = self.swarms[swarm_idx]
                 fitness = self.fitness[swarm_idx]
-                best_fitness = self.best_fitness[swarm_idx]
-                worst_fitness = fitness[np.argmax(fitness)]
-                best_position = self.best_positions[swarm_idx]
+                idx = np.argsort(fitness)
+                worst = fitness[idx[-1]]
+                best = fitness[idx[0]]
                 for j in range(self.swarm_size):
+                    current_fitness = self.function(swarm[j])
                     if j <= int(self.swarm_size / 2):
                         self.weights[swarm_idx][j] = (
                             1 + np.random.rand() *
                             np.log10(np.maximum(
-                                (best_fitness - fitness[j]) /
-                                (best_fitness - worst_fitness + self.epsilon),
+                                (best - current_fitness) /
+                                (best - worst + self.epsilon),
                                 self.epsilon) + 1)
                         )
                     else:
                         self.weights[swarm_idx][j] = (
                              1 - np.random.rand() *
                              np.log10(np.maximum(
-                                 (best_fitness - fitness[j]) /
-                                 (best_fitness - worst_fitness + self.epsilon),
+                                 (best - current_fitness) /
+                                 (best - worst + self.epsilon),
                                  self.epsilon) + 1)
                         )
                 a = np.arctanh(-((i + 1) / self.iterations) + 1)
@@ -66,13 +69,13 @@ class MultiSwarmSMA:
                         vc = np.random.uniform(-1, 1, size=self.dimension)
                         if np.random.random() < p:
                             index1, index2 = np.random.choice(
-                                list(set(range(0, self.swarm_size))-{j}),
+                                list(set(range(0, self.swarm_size)) - {j}),
                                 size=2, replace=False
                             )
                             new_position = (
                                 self.global_best_position + vb *
-                                (self.weights[swarm_idx][j] * swarm[index1]
-                                 - swarm[index2])
+                                (self.weights[swarm_idx][j] * swarm[index1] -
+                                 swarm[index2])
                             )
                         else:
                             new_position = vc * swarm[j]
@@ -84,22 +87,37 @@ class MultiSwarmSMA:
                     if new_fitness < fitness[j]:
                         swarm[j] = new_position
                         fitness[j] = new_fitness
-                        if new_fitness < best_fitness:
-                            best_fitness = new_fitness
-                            best_position = new_position
+                        if new_fitness < self.best_fitness[swarm_idx]:
+                            self.best_fitness[swarm_idx] = new_fitness
+                            self.best_positions[swarm_idx] = new_position
                             if new_fitness < self.global_best_fitness:
                                 self.global_best_fitness = new_fitness
                                 self.global_best_position = new_position
 
-                self.best_fitness[swarm_idx] = best_fitness
-                self.best_positions[swarm_idx] = best_position
                 self.fitness[swarm_idx] = fitness
                 self.swarms[swarm_idx] = swarm
 
-            for swarm in self.swarms:
-                for j in range(self.swarm_size):
-                    swarm[j] += 0.1 * (self.global_best_position - swarm[j])
-                    swarm[j] = np.clip(swarm[j], self.scope[0], self.scope[1])
+            for swarm_idx in range(self.num_swarms - 1):
+                best_f1 = self.best_fitness[swarm_idx]
+                best_f2 = self.best_fitness[swarm_idx + 1]
+                if abs(best_f1 - best_f2) > self.migration_threshold:
+                    migration_rate = (
+                            abs(best_f1 - best_f2) / max(best_f1, best_f2)
+                    )
+                    num_migrate = int(
+                        max(1, min(migration_rate * self.swarm_size,
+                                   self.swarm_size))
+                    )
+                    if best_f1 > best_f2:
+                        source_swarm_idx, target_swarm_idx = (
+                            swarm_idx, swarm_idx + 1
+                        )
+                    else:
+                        source_swarm_idx, target_swarm_idx = (
+                            swarm_idx + 1, swarm_idx
+                        )
+                    migrants = self.swarms[source_swarm_idx][:num_migrate]
+                    self.swarms[target_swarm_idx][-num_migrate:] = migrants
 
             best_values_per_iteration.append(self.global_best_fitness)
             if self.verbose and ((i + 1) % 100 == 0 or i == 0):
@@ -110,9 +128,9 @@ class MultiSwarmSMA:
                 best_values_per_iteration)
 
 
-fun, bounds = choose_fun(1)
+fun, bounds = choose_fun(2)
 mssma = MultiSwarmSMA(
-    fun, bounds, 30, 10, 100, 10, 0.6, False
+    fun, bounds, 30, 10, 100, 10, 0.6, 0.6, False
 )
 best_pos, best_fit, _ = mssma.optimize()
 print("Best Position:", best_pos)
